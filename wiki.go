46377a9a02d8779001d9c1e39dd29f8f90daf0f4
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-08-07 14:47:00 -0300

    add saveHandler

--- wiki.go
+++ wiki.go
@@ -60,10 +60,18 @@ func readerTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t.Execute(w, p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8180", nil))
 }
